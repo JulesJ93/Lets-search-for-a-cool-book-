34d9018a08183f07efa63f980df056daec0d47b7
Jules Joseph <julesj93@gmail.com>
2022-12-12 18:23:11 -0500

    adding const new apollo client  function

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -30,3 +30,37 @@ const authLink = setContext((_, { headers }) => {
     },
   };
 });
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({}),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route 
+            path='/' 
+            element={<SearchBooks />} 
+          />
+          <Route 
+            path='/saved' 
+            element={<SavedBooks />} 
+          />
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          />
+        </Routes>
+      </>
+    </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
