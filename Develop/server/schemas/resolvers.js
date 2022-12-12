85bb9f820463cc0bea9a23a7d41093fd1e2dc19a
Jules Joseph <julesj93@gmail.com>
2022-12-12 18:43:21 -0500

    adding savebook mutation and removeBook

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -33,4 +33,32 @@ const resolvers = {
         const token = signToken(user);
         return { token, user };
       },
-      
+
+      saveBook: async (parent, { bookData }, context) => {
+        if (context.user) {
+            const updatedUser = await User
+                .findOneAndUpdate(
+                    { _id: context.user._id }, 
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true, runValidators: true  },
+                )
+            return updatedUser;
+        };
+        throw new AuthenticationError("You must be logged in to save books!");
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true },
+            );
+            return updatedUser;
+        };
+        throw new AuthenticationError("You must be logged in to delete books!");
+    }
+},
+};
+
+module.exports = resolvers;
