0a25d6eb8b211f7da5e5f92c358751302a93ecd4
Jules Joseph <julesj93@gmail.com>
2022-12-12 18:41:39 -0500

    added mutations for login and add user

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,36 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({_id: context.user._id}).select('-__v -password');
+      }
+      throw new AuthenticationError("You must be logged in!");
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AuthenticationError("No user found with this email address");
+        };
+
+        const correctPW = await user.isCorrectPassword(password);
+        if (!correctPW) {
+            throw new AuthenticationError("Incorrect login credentials!");
+        };
+
+        const token = signToken(user);
+        return { token, user };
+    },
+    
+    addUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+      },
+      
